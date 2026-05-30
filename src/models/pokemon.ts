bc2c336f9725812933f717957d7bf40a937d0135
rogerioweber <rogerioweber97@hotmail.com>
2026-05-30 11:41:01 -0300

    feat: criacao do buscarPokemon da API, criacao da classe Pokemon e quais dados retornar

---
+++ src/models/pokemon.ts
@@ -0,0 +1,33 @@
+export class Pokemon {
+  constructor(
+    public id: number,
+    public name: string,
+    public types: string[],
+    public hp: number,
+    public attack: number,
+    public defense: number,
+  ) {}
+
+  static pokemonDaApi(pokemon: any): Pokemon {
+    const types = pokemon.types.map((pokemonType: any) => pokemonType.type.name);
+
+    const hp = pokemon.stats.find((pokemonStat: any) => pokemonStat.stat.name === "hp");
+
+    const attack = pokemon.stats.find(
+      (pokemonStat: any) => pokemonStat.stat.name === "attack",
+    );
+
+    const defense = pokemon.stats.find(
+      (pokemonStat: any) => pokemonStat.stat.name === "defense",
+    );
+
+    return new Pokemon(
+      pokemon.id,
+      pokemon.name,
+      types,
+      hp.base_stat,
+      attack.base_stat,
+      defense.base_stat,
+    );
+  }
+}
